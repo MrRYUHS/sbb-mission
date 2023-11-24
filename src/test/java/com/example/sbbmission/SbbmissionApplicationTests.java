9170d217167be6b3129266a58d9341a043e21b14
HyeongSoo <rhsrsy123@gmail.com>
2023-11-24 18:16:30 +0900

    sbbmission

---
+++ src/test/java/com/example/sbbmission/SbbmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
